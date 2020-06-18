c97c1464a3613417a6defc8c1f409b7e4a69a4df
Abhimanyu Saraswat <swt.abhimanyu.31@gmail.com>
2020-06-18 17:03:26 +0530

    Minor changes

--- selenium.js
+++ selenium.js
@@ -6,7 +6,7 @@ let login, email, pwd;
 let credentialsFile = process.argv[2];
 let bldr = new swd.Builder();
 let driver = bldr.forBrowser("chrome").build();
-let text = "Aishwarya Mohanraj";
+let channelName,emailTo,sortBy;
 let goToUrl;
 let views,name;
 (async function () {
@@ -16,15 +16,17 @@ let views,name;
         login = credentials.login;
         pwd = credentials.pwd;
         email = credentials.email;
-        //let pageWillBeOpenedP = await driver.get("https://stackoverflow.com/users/login?ssrc=head&returnurl=https%3a%2f%2fstackoverflow.com%2f");
-        // await pageWillBeOpenedP;
+        channelName=credentials.channel;
+        emailTo=credentials.emailto;
+
+        await driver.manage().window().maximize();
+
+        
         await driver.manage().setTimeouts({
             implicit: 40000,
             pageLoad: 40000
         });
-        //let openGoogle = (await driver).findElement(swd.By.css("button[data-oauthserver='https://accounts.google.com/o/oauth2/auth']"));
-        //await openGoogle.click();
-
+        
         let openYoutube = await driver.get("https://www.youtube.com");
 
         await signInYoutube();
@@ -35,33 +37,25 @@ let views,name;
         await nextButton.click();
         let pswdSearchBox = await driver.findElement(swd.By.css("input[name='password']"));
         await pswdSearchBox.sendKeys(pwd);
-
         let signInButton = await driver.findElement(swd.By.css("#passwordNext"));
         await signInButton.click();
 
-
-        
-
         let searchElement = await driver.findElement(swd.By.css(".ytd-searchbox-spt>input"));
-        // console.log(searchElement);identifierNext
-        await searchElement.sendKeys(text + swd.Key.ENTER);
+        await searchElement.sendKeys(channelName + swd.Key.ENTER);
+        
         let channel = await driver.findElement(swd.By.css("#main-link"));
         await channel.click();
+        
         let videosTab = await driver.findElements(swd.By.css(".tab-content"));
-        //console.log(videosTab[1]);
         await videosTab[1].click();
+        
         await sort();
+        
         let videos = await driver.findElements(swd.By.css(".yt-simple-endpoint.style-scope.ytd-grid-video-renderer"));
         console.log(videos.length);
-        // for (var i = 0; i < videos.length; i++) {
-        //     console.log(videos[i]);
-            // console.log('opening video ', i);
-            await like(videos[0]);
-        //     console.log('like fn for video ', i);
-        //     console.log('closing video ', i);
-        //     await driver.get(goToUrl);
-        // }
-
+        
+        await likeAndGetData(videos[0]);
+        
         await sendMail();
 
     } catch (err) {
@@ -73,11 +67,12 @@ let views,name;
 async function sort(){
   let dropdown=await driver.findElements(swd.By.css('#label-icon'));
   await dropdown[1].click();
-  let mostPopular=await driver.findElements(swd.By.css('#menu> a:nth-child(1)'));
-  await mostPopular[1].click();
+    let mostPopular=await driver.findElements(swd.By.css('#menu >a:nth-child(1)'));
+    await mostPopular[1].click();
+  
 }
 
-async function like(video) {
+async function likeAndGetData(video) {
     try {
         await video.click();
         goToUrl = await driver.getCurrentUrl();
@@ -115,17 +110,17 @@ async function signInYoutube() {
 }
 
 async function sendMail(){
+        
     let inboxPage=await driver.get("https://mail.google.com/mail/u/0/");
     let compose=await driver.findElement(swd.By.css('.aic div[role="button"]'));
     await compose.click();
-    let emailTo=await driver.findElement(swd.By.css('textarea[name="to"]'));
-    await emailTo.sendKeys("swt.abhimanyu.31@gmail.com");
+    let sendTo=await driver.findElement(swd.By.css('textarea[name="to"]'));
+        await sendTo.sendKeys(emailTo); 
     let subject=await driver.findElement(swd.By.css('input[name="subjectbox"]'));
-    await subject.sendKeys(`Check the most popular video of ${text}`);
+    await subject.sendKeys(`Check the most popular video of ${channelName}`);
     let message=await driver.findElement(swd.By.css(' div.tS-tW[role="textbox"]'));
-    await message.sendKeys(`Name of video -> ${name}`,swd.Key.ENTER,`Total views on video = ${views}`,swd.Key.ENTER,'Click on the link to go to the video', goToUrl);
+    await message.sendKeys(`Name of video -> ${name}`,swd.Key.ENTER,`Total views on video = ${views}`,swd.Key.ENTER,'Click on the link to go to the video',swd.Key.ENTER, goToUrl);
     console.log(`Total views on video = ${views}`);
-    await message.sendKeys(goToUrl);
-    let send=await driver.findElement(swd.By.css('.aoO'));
-    await send.click();
+    // let send=await driver.findElement(swd.By.css('.aoO'));
+    // await send.click();
 }
